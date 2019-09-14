4fd7852bdad64d3ab6037adf0da39ccd8e464548
sofiacavallo <sofia.c.cavallo@gmail.com>
2019-09-13 20:57:48 -0400

    Update with Jack 9/11 office hours

--- app.py
+++ app.py
@@ -27,14 +27,14 @@ app = Flask(__name__)
 # Base.prepare(db.engine, reflect=True)
 
 # # Save references to each table
-# states_metadata = Base.classes.gunviolence_table
+# states_metadata = Base.classes.gunviolence_db
 # states = Base.classes.states
 
 # ABLE TO QUERY TABLE, CODE IS WORKING. JUST ADD CSV AND CONVERT TO JSON
 # DATABASE NAME = gunviolence_db_sc
-# TABLE IN DATABASE = gunviolence_table
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/gunviolence_db_sc")
-df_gun_violence = pd.read_sql('select * from gunviolence_table', engine)
+# TABLE IN DATABASE = gunviolence_db
+engine = create_engine("sqlite:///db/gunviolence_db.sqlite")
+df_gun_violence = pd.read_sql('select * from gunviolence_db', engine)
 print(len(df_gun_violence))
 
 @app.route("/")
@@ -47,11 +47,14 @@ def list_states():
     """Return a list of states."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(gunviolence_table).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    engine = create_engine("sqlite:///db/gunviolence_db.sqlite")
+    df_gun_violence = pd.read_sql('select * from gunviolence_db', engine)
+    
+    stmt = list(df_gun_violence['State'].unique())
+    # df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (state names)
-    return jsonify(list(df.columns)[2:])
+    return jsonify(stmt)
 
 # METADATA TABLE: Create a summary of gun violence by state. Take relevant information only
 # Eg.: exclude "line_number" and "operations" data. 
@@ -59,27 +62,46 @@ def list_states():
 @app.route("/metadata/<state>")
 def gunviolence_metadata(state):
     """Return the gun violence data summary (metadata) for a given state."""
-    sel = [
-        gunviolence_table.State,
-        gunviolence_table.year,
-        gunviolence_table.Incident_ID,
-        gunviolence_table.Number_Killed,
-        gunviolence_table.Number_Injured,
-    ]
+    # sel = [
+    #     gunviolence_db.State,
+    #     gunviolence_db.year,
+    #     gunviolence_db.Incident_ID,
+    #     gunviolence_db.Number_Killed,
+    #     gunviolence_db.Number_Injured,
+    # ]
 
-    results = db.session.query(*sel).filter(gunviolence_table.State == state).all()
+    # results = db.session.query(*sel).filter(gunviolence_db.State == state).all()
+    engine = create_engine("sqlite:///gunviolence_db.sqlite")
+    df_gun_violence = pd.read_sql(f"select * from gunviolence_db where State = '{state}' ", engine)
+    results = [
+        list(df_gun_violence['State']),
+        list(df_gun_violence.year),
+        list(df_gun_violence['Incident ID'])
+    ]
     print('results:', results)
 
+    # [['Wisconsin', 'Wisconsin', 'Wisconsin', 'Wisconsin', 'Wisconsin'], ['2018', '2018', '2018', '2018', '2018'], ['1289543', '1300872', '1290535', '1289545', '1292121']]
+
+
+# WORK IN JUPYTER NOTEBOOK TO CREATE JSON 
     # Create a dictionary entry for each row of gun violence summary data (metadata)
+    # { 'year' : 2018
+    #   'incident_counts': "IncidentID".count,
+    #   'number_killed': " " 
+        ''
+    # }
+    
+    
     gunviolence_metadata = {}
     for result in results:
         print(result)
+        # 
         gunviolence_metadata["year"] = result[0]
-        gunviolence_metadata["Incident_ID"].count = result[1]
-        gunviolence_metadata["Number_Killed"].sum = result[2]
-        gunviolence_metadata["Number_Injured"].sum = result[3]
+        gunviolence_metadata["Incident_ID"] = result[1]
+        # gunviolence_metadata["Number_Killed"].sum = result[2]
+        # gunviolence_metadata["Number_Injured"].sum = result[3]
 
-    print(gunviolence_metadata)
+    # print(gunviolence_metadata)
     return jsonify(gunviolence_metadata)
 
 # Create app route to pull all data points for charts.
